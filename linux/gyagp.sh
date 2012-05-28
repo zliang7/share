5dde70c39c1a57d2ce294ae8de94bb3b1432a204
Yang <yang.gu@intel.com>
2012-05-28 17:01:28 +0800

    Modify gyagp.sh

---
+++ linux/gyagp.sh
@@ -0,0 +1,10 @@
+alias cd1="cd .."
+alias cd2="cd ../.."
+alias cd3="cd ../../.."
+alias cd4="cd ../../../.."
+alias cd5="cd ../../../../.."
+
+export http_proxy=http://proxy-shz.intel.com:911
+export PATH=/workspace/project/chromium/depot_tools:$PATH
+
+export LC_ALL=en_US.UTF-8

6a973e79572da81f7707bc92223fca826326b843
Yang Gu <yang.gu@intel.com>
2013-03-04 13:39:47 +0800

    ninja_build.py: Make it work for both Windows and Linux

--- common/ninja_build.py
+++ common/ninja_build.py
@@ -1,70 +1,158 @@
 #/usr/bin/python
 
-import os;
-import commands;
-from optparse import OptionParser;
+# Description: Script to sync and build Chromium
+# usage:
+# python ninja_build.py -g "sync"
+# python ninja_build.py -g "sync --force"
+# python ninja_build.py -g "runhooks"
+# python ninja_build.py -t release
 
+import os
+import commands
+from optparse import OptionParser
+import platform
+import re
+
+# Global variables
+rootDir = ''
+srcDir = ''
+system = platform.system()
+
+def isSystem(name):
+    if system == name:
+        return True
+    else:
+        return False
+        
+def isWindows():
+    if isSystem('Windows'):
+        return True
+    else:
+        return False
+
+def isLinux():
+    if isSystem('Linux'):
+        return True
+    else:
+        return False
+        
 def info(msg):
-    print "[INFO] " + msg + ".";
+    print "[INFO] " + msg + "."
 
 def error(msg):
-    print "[ERROR] " + msg + "!";
+    print "[ERROR] " + msg + "!"
 
 def hasOutDir(name):
-    outputDir = options.srcDir + "/out/" + buildTypeStr;
-    if not os.path.exists(outputDir):
-        info(outputDir + " doesn't exist. Will create the directory for you and perform a clean build");
-        commands.getstatusoutput("mkdir -p " + outputDir);
-        return False;
+    if not os.path.exists(srcDir + "/out/" + name):
+        info(name + " directory doesn't exist. Will create the directory for you and perform a clean build")
+        os.chdir(srcDir)
+        os.mkdir('out')
+        os.chdir(srcDir + '/out')
+        os.mkdir(name)
+        return False
 
-    return True;
+    return True
 
-if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-t", "--build-type", dest="buildType", help="assign the build type", metavar="DEBUG|RELEASE|ALL", default="Debug");
-    parser.add_option("-d", "--src-dir", dest="srcDir", help="assign src directory of Chromium", metavar="SRCDIR", default="/workspace/project/chromium/git_upstream/src");
-    parser.add_option("-s", "--build-system", dest="buildSystem", help="assign build system", metavar="ninja", default="ninja");
-    parser.add_option("-c", "--clean-build", action="store_true", dest="cleanBuild", help="need a clean build", default=False);
-    (options, args) = parser.parse_args();
+def gclient(options):
+    if options.gclient == '':
+        return()
 
-    if options.buildType.upper() == "DEBUG":
-        buildTypeStr = "Debug";
-    elif options.buildType.upper() == "RELEASE":
-        buildTypeStr = "Release";
-    elif options.buildType.upper() == "ALL":
-        buildTypeStr = "All";
+    os.chdir(rootDir)
+    if isWindows():
+        cmd = 'd:/user/ygu5/project/chromium/depot_tools/gclient'
     else:
-        error("The build type is wrong");
-        exit -1;
+        cmd = 'gclient'
         
-    if options.buildSystem != "ninja":
-        error("Currently only the build system ninja is supported");
-        exit -1;    
+    cmd = cmd + ' ' + options.gclient    
+    info(cmd)
+    print os.system(cmd)
     
-    if buildTypeStr == "All":
+def build(options):   
+    if options.buildType == '':
+        return()
+    
+    if options.buildType.upper() == "DEBUG":
+        buildType = "Debug"
+    elif options.buildType.upper() == "RELEASE":
+        buildType = "Release"
+    elif options.buildType.upper() == "ALL":
+        buildType = "All"
+
+    cleanBuild = options.cleanBuild    
+    if buildType == "All":
         if not hasOutDir("Debug") and not hasOutDir("Release"):
-            options.cleanBuild = True;
+            cleanBuild = True
     else:
-        if not hasOutDir(buildTypeStr):
-            options.cleanBuild = True;
-
+        if not hasOutDir(buildType):
+            cleanBuild = True
     
-    print "== Build Environment ==";
-    print "Directory of src: " + options.srcDir;
-    print "Build type: " + options.buildType;
-    print "Build system: " + options.buildSystem;
-    print "Need clean build: " + str(options.cleanBuild);
+    print "== Build Environment =="
+    print "Directory of src: " + srcDir
+    print "Build type: " + buildType
+    print "Build system: Ninja"
+    print "Need clean build: " + str(cleanBuild)
+    print "System: " + system
     print "======================="
-
-    os.chdir(options.srcDir);
-    os.putenv("GYP_GENERATORS", "ninja");
-    os.putenv("GYP_DEFINES", "werror= disable_nacl=1 component=shared_library enable_svg=0");
-    if options.cleanBuild:
-        commands.getstatusoutput("build/gyp_chromium");
+        
+    os.chdir(srcDir)
+        
+    if cleanBuild:
+        cmd = 'python build/gyp_chromium'
+        os.system(cmd)
     
-    if buildTypeStr == "Debug" or buildTypeStr == "All":
-        os.system("time " + options.buildSystem + " -C out/Debug chrome");
+    if buildType == "Debug" or buildType == "All":
+        os.system("ninja -C out/Debug chrome")
 
-    if buildTypeStr == "Release" or buildTypeStr == "All":
-        os.system("time " + options.buildSystem + " -C out/Release chrome");
+    if buildType == "Release" or buildType == "All":
+        os.system("ninja -C out/Release chrome")
+    
+if __name__ == "__main__":
+    # System sanity check
+    if not isWindows() and not isLinux():
+        error('Current system is not suppported')
+        quit()
+        
+    # Handle options
+    parser = OptionParser()
+    parser.add_option("-g", "--gclient", dest="gclient", help="Update source code", default='')
+    parser.add_option("-c", "--clean-build", action="store_true", dest="cleanBuild", help="need a clean build", default=False)
+    parser.add_option("-t", "--build-type", dest="buildType", help="assign the build type", metavar="DEBUG|RELEASE|ALL", default='')
+    parser.add_option("-d", "--root-dir", dest="rootDir", help="assign root directory of Chromium", metavar="ROOTDIR", default='')
+    (options, args) = parser.parse_args()
+
+    # Global variables
+    if options.rootDir == '':
+        if isWindows():
+            rootDir = "d:/user/ygu5/project/chromium"
+        else:
+            rootDir = "/workspace/project/chromium/git_upstream"
+    else:
+        rootDir = options.rootDir
+        
+    srcDir = rootDir + '/src'
+
+    os.putenv('http_proxy', 'http://proxy-shz.intel.com:911')
+    os.putenv('https_proxy', 'https://proxy-shz.intel.com:911')
+    
+    if isWindows():
+        path = os.getenv("PATH")
+        p = re.compile('depot_tools')
+        if not p.search(path):
+            path = "d:\user\ygu5\project\chromium\depot_tools;" + path
+            os.putenv("PATH", path)        
+    
+    os.putenv("GYP_GENERATORS", "ninja")
+    if isWindows():
+        os.putenv('GYP_DEFINES', 'werror= disable_nacl=1 component=shared_library enable_svg=0 windows_sdk_path="d:/user/ygu5/project/chromium/win_toolchain/win8sdk"')
+        os.putenv("GYP_MSVS_VERSION", "2010e")
+        os.putenv("GYP_MSVS_OVERRIDE_PATH", "d:/user/ygu5/project/chromium/win_toolchain")
+        os.putenv("WDK_DIR", "d:/user/ygu5/project/chromium/win_toolchain/WDK")
+        os.putenv("DXSDK_DIR", "d:/user/ygu5/project/chromium/win_toolchain/DXSDK")
+        os.putenv("WindowsSDKDir", "d:/user/ygu5/project/chromium/win_toolchain/win8sdk")
+    else:
+        os.putenv("GYP_DEFINES", "werror= disable_nacl=1 component=shared_library enable_svg=0")    
+    
+    # Real work
+    gclient(options)
+    build(options)
     

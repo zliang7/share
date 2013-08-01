370342bb0f9391191fded6c511b18fd77f2ca3dc
Yang Gu <yang.gu@intel.com>
2013-08-01 23:01:07 +0800

    album: Refactor code to be much simpler

--- windows/album/album.py
+++ windows/album/album.py
@@ -7,20 +7,20 @@ import os
 import sys
 import shutil
 
-scriptDir = sys.path[0]
-checkDirs = [u'11月']
+script_dir = sys.path[0]
+check_dirs = [u'12月']
 
-originDir = u'e:/picture/origin/'
-storageDir = 'e:/storage/'
-googlePlusStorageDir = storageDir + 'GooglePlus/'
-skyDriveStorageDir = storageDir + u'SkyDrive/顾诗云/'
+origin_dir = u'e:/picture/origin/'
+storage_dir = 'e:/storage/'
+googleplus_storage_dir = storage_dir + 'GooglePlus/'
+skydrive_storage_dir = storage_dir + u'SkyDrive/顾诗云/'
 
 ffmpeg = 'E:/software/active/ffmpeg/bin/ffmpeg.exe'
 
 def info(msg):
     print '[INFO] ' + msg + '.'
 
-def hasFile(dir, name):
+def has_file(dir, name):
     files = os.listdir(dir)
     for file in files:
         if file == name:
@@ -35,59 +35,54 @@ def lock(name):
 def unlock(name):
     os.remove(name)
 
-def originToGooglePlus():
-    for checkDir in checkDirs:
-        originCheckDir = originDir + checkDir + '/'
-        if not os.path.exists(originCheckDir):
+def src_to_dest(src_dir, dest_dir):
+    for check_dir in check_dirs:
+        src_check_dir = src_dir + check_dir + '/'
+        if not os.path.exists(src_check_dir):
             continue
 
-        googlePlusCheckDir = googlePlusStorageDir + checkDir + '/'
-        if not os.path.exists(googlePlusCheckDir):
-            os.mkdir(googlePlusCheckDir)
-        originFiles = os.listdir(originCheckDir)
-        for originFile in originFiles:
-            if originFile[-4:] == '.mp4' and not hasFile(googlePlusCheckDir, originFile):
-                info('Copy file ' + originCheckDir + originFile + ' to ' + googlePlusCheckDir + originFile)
-                shutil.copyfile(originCheckDir + originFile, googlePlusCheckDir + originFile)
+        dest_check_dir = dest_dir + check_dir + '/'
+        if not os.path.exists(dest_check_dir):
+            os.mkdir(dest_check_dir)
+        src_files = os.listdir(src_check_dir)
+        for src_file in src_files:
+            src_file_suffix = src_file[-4:].upper()
+            src_file_path = src_check_dir + src_file
+            if src_file_suffix == '.MOV':
+                dest_file = src_file.replace('.MOV', '.MP4')
+            else:
+                dest_file = src_file
+            dest_file_path= dest_check_dir + dest_file
+
+            if os.path.exists(dest_file_path):
                 continue
 
-            if originFile[-4:] == '.MOV':
-                destFile = originFile.replace('.MOV', '.MP4')
-                if hasFile(googlePlusCheckDir, destFile):
-                    continue
-                command = (ffmpeg + ' -i ' + originCheckDir + originFile + ' -qscale 0 -s hd1080 -f mp4 ' + destFile + ' 2>>NUL').encode(sys.getfilesystemencoding())
-                info('Convert file ' + originCheckDir + originFile + ' to ' + googlePlusCheckDir + destFile)
+            if src_file_suffix == '.JPG':
+                info('Copy file ' + src_file_path + ' to ' + dest_file_path)
+                shutil.copyfile(src_file_path, dest_file_path)
+            elif src_file_suffix == '.MP4':
+                if src_dir == origin_dir:
+                    info('Copy file ' + src_file_path + ' to ' + dest_file_path)
+                    shutil.copyfile(src_file_path, dest_file_path)
+                else:
+                    command = (ffmpeg + ' -i ' + src_file_path + ' -qscale 0 -s hd720 -f mp4 ' + dest_file + ' 2>>NUL').encode(sys.getfilesystemencoding())
+                    info('Convert file ' + src_file_path + ' (1080P) to ' + dest_file_path + ' (720P)')
+                    os.system(command)
+                    os.rename(dest_file, dest_file_path)
+            elif src_file_suffix == '.MOV':
+                command = (ffmpeg + ' -i ' + src_file_path + ' -qscale 0 -s hd1080 -f mp4 ' + dest_file + ' 2>>NUL').encode(sys.getfilesystemencoding())
+                info('Convert file ' + src_file_path + ' to ' + dest_file_path)
                 os.system(command)
-                os.rename(destFile, googlePlusCheckDir + destFile)
-
-def googlePlusToSkyDrive():
-    print 'haha'
-    for checkDir in checkDirs:
-        googlePlusCheckDir = googlePlusStorageDir + checkDir + '/'
-        skyDriveCheckDir = skyDriveStorageDir + checkDir + '/'
-        if not os.path.exists(skyDriveCheckDir):
-            os.mkdir(skyDriveCheckDir)
-
-        googlePlusFiles = os.listdir(googlePlusCheckDir)
-        for googlePlusFile in googlePlusFiles:
-            if googlePlusFile[-4:].lower() == '.jpg' and not hasFile(skyDriveCheckDir, googlePlusFile):
-                command = ('cp ' + googlePlusCheckDir  + googlePlusFile + ' ' + skyDriveCheckDir + googlePlusFile).encode(sys.getfilesystemencoding())
-                info('Copy file ' + googlePlusCheckDir + googlePlusFile + ' to ' + skyDriveCheckDir + googlePlusFile)
-                os.system(command)
-            elif googlePlusFile[-4:].lower() == '.mp4' and not hasFile(skyDriveCheckDir, googlePlusFile):
-                command = (ffmpeg + ' -i ' + googlePlusCheckDir  + googlePlusFile + ' -qscale 0 -s hd720 -f mp4 ' + googlePlusFile + ' 2>>NUL').encode(sys.getfilesystemencoding())
-                info('Convert file ' + googlePlusCheckDir + googlePlusFile + ' to ' + skyDriveCheckDir + googlePlusFile)
-                os.system(command)
-                os.rename(googlePlusFile, skyDriveCheckDir + googlePlusFile)
+                os.rename(dest_file, dest_file_path)
 
 if __name__ == '__main__':
-    os.chdir(scriptDir)
-    lockFile = 'lock'
-    if hasFile(scriptDir, lockFile):
+    os.chdir(script_dir)
+    lock_file = 'lock'
+    if has_file(script_dir, lock_file):
         quit()
 
-    lock(lockFile)
-    originToGooglePlus()
-    googlePlusToSkyDrive()
-    unlock(lockFile)
+    #lock(lock_file)
+    src_to_dest(origin_dir, googleplus_storage_dir)
+    src_to_dest(googleplus_storage_dir, skydrive_storage_dir)
+    #unlock(lock_file)
 

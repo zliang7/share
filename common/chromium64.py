290096f3e375ff03130586b1ee6cb85c76419930
Yang Gu <yang.gu@intel.com>
2013-11-22 19:07:51 +0800

    chromium64.py: Enhance mk64

--- common/chromium64.py
+++ common/chromium64.py
@@ -7,12 +7,17 @@ import platform
 import re
 import sys
 
-script_dir = ''
+root_dir = '/workspace/project/android-ia'
 webview_dir = ''
 args = ''
+projects = []
 
 patches = [
     # Patches borrowed from other groups
+    #git fetch ssh://aia-review.intel.com/platform/system/core refs/changes/00/1700/2 && git cherry-pick FETCH_HEAD # atomic basic change
+    #git fetch ssh://aia-review.intel.com/platform/system/core refs/changes/57/1957/1 && git cherry-pick FETCH_HEAD # atomic
+    #git fetch ssh://aia-review.intel.com/platform/bionic refs/changes/43/1943/1 && git cherry-pick FETCH_HEAD # size_t
+    #git fetch ssh://aia-review.intel.com/platform/frameworks/native refs/changes/50/1950/1 && git cherry-pick FETCH_HEAD # libbinder
 
     # Patches by our own
     'git fetch https://aia-review.intel.com/platform/external/chromium_org refs/changes/29/2329/1 && git checkout FETCH_HEAD',
@@ -30,8 +35,10 @@ def error(msg):
 def cmd(msg):
     print '[COMMAND] ' + msg
 
-def execute(command):
-    cmd(command)
+def execute(command, silent=False):
+    if not silent:
+        cmd(command)
+
     if os.system(command):
         error('Failed to execute')
         quit()
@@ -69,11 +76,19 @@ examples:
     args = parser.parse_args()
 
 def setup():
-    global script_dir, webview_dir
+    global webview_dir, projects
 
-    script_dir = get_script_dir()
-    webview_dir = script_dir + '/external/chromium_org'
+    webview_dir = root_dir + '/external/chromium_org'
     os.chdir(webview_dir)
+
+
+    r = os.popen('find -name ".git"')
+    lines = r.read().split('\n')
+    del lines[len(lines) - 1]
+    for project in lines:
+        project = project.replace('./', '')
+        project = project.replace('.git', '')
+        projects.append(project)
 
 def patch():
     if not args.patch:
@@ -85,7 +100,7 @@ def patch():
         project = match.group(1)
         change = match.group(2)
         command = 'git fetch ssh://aia-review.intel.com/platform/' + project + ' ' + change + ' && git cherry-pick FETCH_HEAD'
-        os.chdir(script_dir + '/' + project)
+        os.chdir(root_dir + '/' + project)
         execute(command)
 
     os.chdir(webview_dir)
@@ -94,36 +109,59 @@ def mk64():
     if not args.mk64:
         return
 
-    backups = [
-        'GypAndroid.linux-x86_64.mk', # good
-        'third_party/openssl/openssl.target.linux-x86_64.mk', # good
-        #./media/media.target.linux-x86_64.mk
-        #third_party/x86inc ./media/media_asm.target.linux-x86_64.mk
-        #refer ./v8/tools/gyp/v8_base.ia32.target.linux-x86_64.mk
-        #refer ./v8/tools/gyp/v8_base.ia32.host.linux-x86_64.mk
-        #refer ./base/base.target.linux-x86_64.mk
-        #refer ./third_party/protobuf/protobuf_lite.target.linux-x86_64.mk
-        #refer ./third_party/protobuf/protobuf_full_do_not_use.host.linux-x86_64.mk
-        #bad ./third_party/yasm/generate_files.host.linux-x86_64.mk
-        #bad ./third_party/yasm/yasm.host.linux-x86_64.mk
-    ]
+    command = 'export CHROME_ANDROID_BUILD_WEBVIEW=1 && . build/android/envsetup.sh --target-arch=x86_64 && android_gyp -Dwerror='
+    #execute(command)
 
+    # Remove all the linux-x86_64 files
+    r = os.popen('find -name "*linux-x86_64.mk"')
+    files = r.read().split('\n')
+    del files[len(files) - 1]
+    for file in files:
+        os.remove(file)
+
+    # Generate x64 files
+    file = open('GypAndroid.mk')
+    lines = file.readlines()
+    file.close()
+
+    fw = open('GypAndroid.linux-x86_64.mk', 'w')
+
+    # auto_x64 -> x64: target->target.linux-x86_64, host->host.linux-x86_64
+    for line in lines:
+        pattern = re.compile('\(LOCAL_PATH\)/(.*)')
+        match = pattern.search(line)
+        if match:
+            auto_x64_file = match.group(1)
+            x64_file = auto_x64_file.replace('target', 'target.linux-x86_64')
+            x64_file = x64_file.replace('host', 'host.linux-x86_64')
+            command = 'cp -f ' + auto_x64_file + ' ' + x64_file
+            execute(command, True)
+
+            line = line.replace('target', 'target.linux-x86_64')
+            line = line.replace('host', 'host.linux-x86_64')
+        fw.write(line)
+
+    fw.close()
+
+    # Check if x86 version has corresponding x86_64 version
+    # x86 -> x64: x86->x86_64, ia32->x86_64
     r = os.popen('find -name "*linux-x86.mk"')
     files = r.read().split('\n')
-    # Remove last empty one
     del files[len(files) - 1]
 
-    for file in files:
-        command = 'cp -f ' + file + ' ' + file.replace('x86', 'x86_64')
-        execute(command)
-        #r = os.popen('grep -c x86 ' + file)
-        #count = int(r.read())
-        #if count > 0:
-        #    print file
+    for x86_file in files:
+        x64_file = x86_file.replace('x86', 'x86_64')
+        x64_file = x64_file.replace('ia32', 'x86_64')
+        if not os.path.exists(x64_file):
+            print 'x64 version does not exist: ' + x86_file
 
-    for file in backups:
-        command = 'cp -f /workspace/topic/x64/' + file.rsplit('/')[-1] + ' ' + file
-        execute(command)
+    info('number of x86 mk: ' + os.popen('find -name "*linux-x86.mk" |wc -l').read())
+    info('number of x64 mk: ' + os.popen('find -name "*linux-x86_64.mk" |wc -l').read())
+
+def check_status():
+    for project in projects:
+        execute('git status ' + project)
+
 
 def build():
     if not args.build:
@@ -136,7 +174,7 @@ def build():
     else:
         command += 'mmm ./'
 
-    command += ' -j16'
+    command += ' -j16 -k'
     execute(command)
 
 if __name__ == '__main__':
@@ -144,4 +182,5 @@ if __name__ == '__main__':
     setup()
     patch()
     mk64()
+    #check_status()
     build()
